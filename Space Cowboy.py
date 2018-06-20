a6f9a6134e325cf8c02a7afbe69ff77c49402d8e
FinalBossSadalbari <acurbina@uc.cl>
2018-06-20 21:21:31 +0200

    Dictionary and user input experiment

--- Space Cowboy.py
+++ Space Cowboy.py
@@ -15,12 +15,19 @@ planets = {
 def SpaceCowboy():
     user_input = input("Welcome, Space Cowboy! Are you ready to explore the universe?...the Star Trek style, of course! (Yes!/Nah)\n")
     if user_input == "Yes!":
+        print ("We can start by identifying planet types! Write the name of a planet here:")
         Planets(),
     if user_input == "Nah":
-        pass;
+        user_input = input("Are you sure? It's fun! (Yes, I'm boring/Fine, show me the Universe)\n")
+        if user_input == "Fine, show me the Universe":
+            print ("We can start by identifying planet types! Write the name of a planet here:")
+            Planets(),
+        if user_input == "Yes, I'm boring":
+            print ("Then I hope you die of boredom :)")
+            pass;
 
 def Planets():
-    user_input = input("Find out the class of your planet. Write a name here:\n")
+    user_input = input()
     if user_input in planets:
         print (planets[user_input]),
     if user_input in planets and planets[user_input] == "Class M" and user_input != "Earth":
@@ -28,5 +35,5 @@ def Planets():
     if user_input in planets and "Class" not in planets[user_input]:
         print ("Undetermined Class"),
     if user_input not in planets:
-        print ("Please write the name of a planet (Example: 'Earth')")
+        print ("Please write the name of a planet (Example: 'Earth'):")
         Planets()
